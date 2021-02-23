7711ead266c8a8346d5c3746a5a10d0e03938e4e
Doug Perez <perezcpt@gmail.com>
2021-02-23 12:37:34 -0800

    Basic prototype build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    /*   var today = new Date(),
+    time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();  */
+    this.state = {
+      seconds: "",
+      minutes: "",
+      hours: "",
+    };
+
+    this.updateTime = this.updateTime.bind(this);
+    this.startTimeTracker = this.startTimeTracker.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateTime();
+    this.startTimeTracker();
+  }
+
+  startTimeTracker() {
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    var today = new Date(),
+      /* time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); */
+      time =
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    this.setState({ currentTime: time });
+    this.setState({ seconds: today.getSeconds() });
+    this.setState({ minutes: today.getMinutes() });
+    this.setState({ hours: today.getHours() });
+  }
+
+  render() {
+    return (
+      <span className="digital-clock">
+        <div>{this.state.hours}:</div>
+        <div>
+          {this.state.minutes < 10
+            ? "0" + this.state.minutes
+            : this.state.minutes}
+          :
+        </div>
+        <div className="secs">
+          {this.state.seconds < 10
+            ? "0" + this.state.seconds
+            : this.state.seconds}
+        </div>
+        <div>{this.state.hours < 12 ? "AM" : "PM"}</div>
+      </span>
+    );
+  }
 }
 
 export default App;
